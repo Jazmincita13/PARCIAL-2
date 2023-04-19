e36e99a484afeaf0853d435e8983439801ad8837
Jazmincita13 <125412889+Jazmincita13@users.noreply.github.com>
2023-04-18 21:04:41 -0600

    Add files via upload

---
+++ PARCIAL 2/PARCIAL 2/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+//Console.WriteLine("Hello, World!");
+
+
+int dato1 = 16;
+string dato2 = "25g";
+int resultado;
+
+try
+{
+    resultado = dato1 + Convert.ToInt32(dato2);
+    Console.WriteLine("El resultado es: " + resultado);
+}
+catch (Exception ex)
+{
+    Console.WriteLine("Lo sentimos no se pudo realizar la operación solicitada");
+}
